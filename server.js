b4c58c04f1582f3e5c73d72a5e6437d2c10b80f9
rdzcn <ardaoezcan@gmail.com>
2024-02-21 15:00:32 +0300

    remove vite and implement simple nodejs server

---
+++ server.js
@@ -0,0 +1,43 @@
+import * as fs from "node:fs";
+import { createServer } from "node:http";
+import * as path from "node:path";
+
+const PORT = 4040;
+
+const MIME_TYPES = {
+  default: "application/octet-stream",
+  html: "text/html; charset=UTF-8",
+  js: "application/javascript",
+  css: "text/css",
+  ico: "image/x-icon",
+};
+
+const STATIC_PATH = path.join(process.cwd());
+
+const toBool = [() => true, () => false];
+
+const prepareFile = async (url) => {
+  const paths = [STATIC_PATH, url];
+  if (url.endsWith("/")) paths.push("index.html");
+  const filePath = path.join(...paths);
+  const pathTraversal = !filePath.startsWith(STATIC_PATH);
+  const exists = await fs.promises.access(filePath).then(...toBool);
+  const found = !pathTraversal && exists;
+  const streamPath = found ? filePath : STATIC_PATH + "/404.html";
+  const ext = path.extname(streamPath).substring(1).toLowerCase();
+  const stream = fs.createReadStream(streamPath);
+  return { found, ext, stream };
+};
+
+const server = createServer(async (req, res) => {
+    const file = await prepareFile(req.url);
+    const statusCode = file.found ? 200 : 404;
+    const mimeType = MIME_TYPES[file.ext] || MIME_TYPES.default;
+    res.writeHead(statusCode, { "Content-Type": mimeType });
+    file.stream.pipe(res);
+    console.log(`${req.method} ${req.url} ${statusCode}`);
+})
+  
+server.listen(PORT);
+
+console.log(`Server running at http://127.0.0.1:${PORT}/`);
