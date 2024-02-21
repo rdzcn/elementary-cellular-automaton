6ee776a1bb18a7ad13b1dbfc12a4b0ddadff71d1
rdzcn <ardaoezcan@gmail.com>
2024-02-21 10:34:23 +0300

    unstyled working version

---
+++ index.js
@@ -0,0 +1,83 @@
+// Utility functions
+function convertToBinary(number) {
+  let binary = number.toString(2);
+  while (binary.length < 8) {
+    binary = "0" + binary;
+  }
+  return binary;
+}
+
+// End of utility functions
+
+//Global variables
+
+const squareWidth = 5;
+const squareHeight = 5;
+const totalCells = window.innerWidth / squareWidth;
+
+let rule = 0;
+let cells = [];
+
+// End of global variables
+
+
+function calculateNewState(left, center, right) {
+  const ruleArray = convertToBinary(rule).split("");
+  const input = `${left}${center}${right}`;
+  const ruleIndex = 7 - parseInt(input, 2);
+  return ruleArray[ruleIndex];
+}
+
+function generateCell() {
+  let nextGen = [];
+  for (let i = 0; i < totalCells; i++) {
+    const left = cells[(i - 1 + totalCells) % totalCells];
+    const right = cells[(i + 1 + totalCells) % totalCells];
+    const center = cells[i];
+    const newState = calculateNewState(left, center, right);
+    nextGen[i] = +newState;
+  }
+  cells = nextGen;
+}
+
+const ruleInput = document.querySelector("#rule-input");
+const generateButton = document.querySelector("#generate");
+
+generateButton.onclick = 
+  function () {
+    let canvas = document.querySelector("#automata");
+    canvas.style.backgroundColor = "#fff";
+    canvas.width = window.innerWidth - 15;
+    canvas.height = 1600;
+    rule = +ruleInput.value;
+    const ctx = canvas.getContext("2d");
+
+    console.log("onClick cells", cells)
+
+    for (let i = 0; i < totalCells; i++) {
+      if (i === Math.ceil(totalCells / 2)) {
+        cells[i] = 1;
+      } else cells[i] = 0;
+      // cells[i] = Math.floor(getRandomNumber(0, 2));
+    }
+
+    let row = 0;
+    const animate = () => {
+      ctx.beginPath();
+      for (let i = 0; i < totalCells; i++) {
+        const x = i * squareWidth;
+        const y = row * squareHeight;
+        ctx.fillStyle = cells[i] ? "#000" : "#fff";
+        ctx.fillRect(x, y, squareWidth, squareHeight);
+        ctx.closePath();
+      }
+      generateCell();
+      if (row < canvas.height / squareHeight) {
+        requestAnimationFrame(animate);
+      }
+      row += 1;
+    };
+
+    animate();
+  }
+
